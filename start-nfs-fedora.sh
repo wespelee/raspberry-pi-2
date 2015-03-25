0a7c4cb7db02cc5f855469c6d2167f003ad6f4a3
Jian-Hui Lee <wespe.ljh@gmail.com>
2015-03-25 18:51:15 +0800

    Change mount root for convenient.

--- start-nfs-fedora.sh
+++ start-nfs-fedora.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
-sudo systemctl stop rpcbind
-sudo systemctl stop nfs-server
-sudo systemctl stop nfs-lock 
-sudo systemctl stop nfs-idmap
+#sudo systemctl stop rpcbind
+#sudo systemctl stop nfs-server
+#sudo systemctl stop nfs-lock 
+#sudo systemctl stop nfs-idmap
+#
+#sudo systemctl start rpcbind
+#sudo systemctl start nfs-server
+#sudo systemctl start nfs-lock 
+#sudo systemctl start nfs-idmap
+#
+#sudo systemctl enable rpcbind
+#sudo systemctl enable nfs-server
+#sudo systemctl enable nfs-lock 
+#sudo systemctl enable nfs-idmap
 
-sudo systemctl start rpcbind
-sudo systemctl start nfs-server
-sudo systemctl start nfs-lock 
-sudo systemctl start nfs-idmap
-
-sudo systemctl enable rpcbind
-sudo systemctl enable nfs-server
-sudo systemctl enable nfs-lock 
-sudo systemctl enable nfs-idmap
+sudo mount -t nfs 192.168.5.74:/ /home/wespe/test_work/raspberry/raspberry-pi-2/rpi-nfs
